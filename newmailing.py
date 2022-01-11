9e13b621d658c07f9786601ec0d43c897fb782b0
minhthuhua72 <73808947+minhthuhua72@users.noreply.github.com>
2022-01-11 20:41:13 +0700

    upload files

---
+++ newmailing.py
@@ -0,0 +1,27 @@
+import smtplib
+from smtplib import SMTP
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+recipients = ["jesshua722@gmail.com"]
+
+def send_email():
+    from_adr = "jesshua722@gmail.com"
+    server = SMTP('smtp.gmail.com', 587)
+    subject= "Send from Rasberry Pi"
+    
+    msg = MIMEMultipart()
+    msg["From"] = from_adr
+    msg["To"] = recipients[0]
+    msg["Subject"] = subject
+    message = "If you want to manually change the light, go to this website: http://192.168.50.199/index.php"
+    msg.attach(MIMEText(message))
+
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login("jesshua722@gmail.com", "KIKI&&KICHI4ever")
+    server.sendmail(from_adr, recipients, msg.as_string())
+    server.quit()
+
+
---
+++ stopwatch.py
@@ -0,0 +1,30 @@
+import time
+import RPi.GPIO as GPIO
+
+second = 0
+minute = 0
+
+# PIN connected to IN1
+relay_pin = 23
+
+# Set mode BCM
+GPIO.setmode(GPIO.BCM) 
+
+# Type of PIN - ouput
+GPIO.setup(relay_pin, GPIO.OUT)
+GPIO.setwarnings(False)
+
+def stop():
+    global second
+    global minute
+    while True:
+        print('\t\t\t\t %d : %d '%(minute,second))
+        time.sleep(1)
+        second+=1
+        if (second == 100):
+            second = 0
+            minute+=1
+            GPIO.output(relay_pin, GPIO.HIGH)
+            break
+    
+        
